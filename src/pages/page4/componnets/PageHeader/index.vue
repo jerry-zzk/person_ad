9e36736e48bb0149adf9fff7bfe85a947e776b1e
lilish <xiangrikuifangao@sohu.com>
2018-09-11 17:29:24 +0800

    黑名单

---
+++ src/pages/page4/componnets/PageHeader/index.vue
@@ -0,0 +1,99 @@
+<template>
+  <el-form
+    :inline="true"
+    :model="form"
+    :rules="rules"
+    ref="form"
+    size="mini"
+    style="margin-bottom: -18px;">
+
+    <el-form-item label="原因配型" prop="type">
+      <el-select
+        v-model="form.type"
+        placeholder="状态选择"
+        style="width: 100px;">
+        <el-option label="电话" value="1"/>
+        <el-option label="邮箱" value="2"/>
+        <el-option label="身份证" value="3"/>
+        <el-option label="地址" value="4"/>
+        <el-option label="姓名" value="5"/>
+      </el-select>
+    </el-form-item>
+
+    <el-form-item label="用户名" prop="user">
+      <el-input
+        v-model="form.user"
+        placeholder="用户"
+        style="width: 100px;"/>
+    </el-form-item>
+
+    <el-form-item label="身份证号" prop="id">
+      <el-input
+        v-model="form.id"
+        placeholder="身份证号"
+        style="width: 120px;"/>
+    </el-form-item>
+
+    <el-form-item label="电话号" prop="phone">
+      <el-input
+        v-model="form.phone"
+        placeholder="电话号"
+        style="width: 120px;"/>
+    </el-form-item>
+
+    <el-form-item>
+      <el-button
+        type="primary"
+        @click="handleFormSubmit">
+        <d2-icon name="search"/>
+        查询
+      </el-button>
+    </el-form-item>
+
+    <el-form-item>
+      <el-button
+        @click="handleFormReset">
+        <d2-icon name="refresh"/>
+        重置
+      </el-button>
+    </el-form-item>
+
+  </el-form>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      form: {
+        type: '1',
+        user: 'hahahaha',
+        key: '',
+        note: ''
+      },
+      rules: {
+        type: [ { required: true, message: '请选择一个状态', trigger: 'change' } ],
+        user: [ { required: true, message: '请输入用户', trigger: 'change' } ]
+      }
+    }
+  },
+  methods: {
+    handleFormSubmit () {
+      this.$refs.form.validate((valid) => {
+        if (valid) {
+          this.$emit('submit', this.form)
+        } else {
+          this.$notify.error({
+            title: '错误',
+            message: '表单校验失败'
+          })
+          return false
+        }
+      })
+    },
+    handleFormReset () {
+      this.$refs.form.resetFields()
+    }
+  }
+}
+</script>
--- src/pages/page4/index.vue
+++ src/pages/page4/index.vue
@@ -1,13 +1,164 @@
 <template>
-  <d2-container>
-    <template slot="header">Page 4 header</template>
-    Hello World
-    <template slot="footer">footer</template>
-  </d2-container>
+	<d2-container>
+		<demo-page-header
+	      slot="header"
+	      @submit="handleSubmit"
+	      ref="header"/>
+		<div>
+		    <ul class="people_list">
+		      	<li class="people_item black_list">
+			        <img src="../index/image/qr@2x.png">
+			        <span class="state phone"><i class="fa fa-phone"></i></span>
+			        <div class="info">
+				        <span>hahahha</span>
+				        <span>11118611108792117</span>
+				        <span>234234@sdsd.com</span>
+			    	</div>
+		      	</li>
+		      	<li class="people_item black_list">
+			        <img src="../index/image/qr@2x.png">
+			        <span class="state phone"><i class="fa fa-phone"></i></span>
+			        <div class="info">
+				        <span>hahahha</span>
+				        <span>11118611108792117</span>
+				        <span>234234@sdsd.com</span>
+			    	</div>
+		      	</li>
+		      	<li class="people_item black_list">
+			        <img src="../index/image/qr@2x.png">
+			        <span class="state id"><i class="fa fa-phone"></i></span>
+			        <div class="info">
+				        <span>hahahha</span>
+				        <span>11118611108792117</span>
+				        <span>234234@sdsd.com</span>
+			    	</div>
+		      	</li>
+		      	<li class="people_item black_list">
+			    	<img src="../index/image/qr@2x.png">
+			    	<span class="state phone"><i class="fa fa-phone"></i></span>
+			        <div class="info">
+				        <span>hahahha</span>
+				        <span>11118611108792117</span>
+				        <span>234234@sdsd.com</span>
+		    		</div>
+		      	</li>
+		    </ul>
+	 	</div>
+    	<demo-page-footer
+	      slot="footer"
+	      :current="page.current"
+	      :size="page.size"
+	      :total="page.total"
+	      @change="handlePaginationChange"/>
+  	</d2-container>
 </template>
-
+<style type="text/css" scope>
+  .people_list{
+    list-style: none;
+    margin:0px;
+    padding:0px;
+  }
+  .people_item .state{
+  	position: absolute;
+  	right:-40px;
+  	top:-40px;
+  	width:80px;
+  	height:80px;
+	background: red;
+	transform: rotate(45deg);
+  }
+  .people_item .state i{
+  	color:white;
+  	line-height: 50px;
+  	transform: rotate(-45deg);
+  	margin-top: 40px;
+    margin-left: 30px;
+    font-size: 28px;
+  }
+  .people_item{
+  	overflow: hidden;
+    float:left;
+    display: block;
+    border:1px solid black;
+    box-shadow: 1px 1px 5px 1px rgba(0,0,0,0.5);
+    width:200px;
+    height:300px;
+    position: relative;
+    cursor:pointer;
+    margin:10px;
+  }
+  .people_item img{
+  	width:150px;
+  	position:absolute;
+  	left:50%;
+  	margin-left:-75px;
+  	top:25px;
+  }
+  .people_item .info{
+  	margin-top:200px;
+  	text-align:center;
+  }
+  .people_item span{
+  	display: block;
+  	margin-bottom:10px;
+  }
+</style>
 <script>
+import { BusinessTable1List } from '@/api/demo/business/table/1'
 export default {
-  name: 'page4'
+  // name 值和本页的 $route.name 一致才可以缓存页面
+  name: 'demo-business-table-1',
+  components: {
+    'DemoPageHeader': () => import('./componnets/PageHeader'),
+    'DemoPageFooter': () => import('./componnets/PageFooter')
+  },
+  data () {
+    return {
+      table: [],
+      page: {
+        current: 1,
+        size: 100,
+        total: 0
+      }
+    }
+  },
+  methods: {
+    handlePaginationChange (val) {
+      this.$notify({
+        title: '分页变化',
+        message: `当前第${val.current}页 共${val.total}条 每页${val.size}条`
+      })
+      this.page = val
+      // nextTick 只是为了优化示例中 notify 的显示
+      this.$nextTick(() => {
+        this.$refs.header.handleFormSubmit()
+      })
+    },
+    handleSubmit (form) {
+      this.loading = true
+      this.$notify({
+        title: '开始请求模拟表格数据'
+      })
+      BusinessTable1List({
+        ...form,
+        page: this.page
+      })
+        .then(res => {
+          this.loading = false
+          this.$notify({
+            title: '模拟表格数据请求完毕'
+          })
+          this.table = res.list
+          this.page = res.page
+        })
+        .catch(err => {
+          this.loading = false
+          this.$notify({
+            title: '模拟表格数据请求异常'
+          })
+          console.log('err', err)
+        })
+    }
+  }
 }
 </script>
