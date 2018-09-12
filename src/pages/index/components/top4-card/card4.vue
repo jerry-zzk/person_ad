e8b7dab2024a01dbf21836719d1f41dabfb7daaa
gcx <970659339@qq.com>
2018-09-12 16:00:05 +0800

    card4

--- src/pages/index/components/top4-card/card4.vue
+++ src/pages/index/components/top4-card/card4.vue
@@ -3,23 +3,30 @@
         <el-row :gutter="10">
             <el-col :span="6">
                 <div class="col col-l">
-                    <p>客户/分组 <b>:</b> <span> {{user}} / {{group}} </span></p>
-                    <p></p>
+                    <h4>客户/分组</h4>
+                    <p><span> {{user}} / {{group}} </span></p>
+                    <div class="icon fa fa-ravelry"></div>
                 </div>
             </el-col>
             <el-col :span="6">
                 <div class="col col-r">
-                    <p>敲诈客户 <b>:</b> <span> {{user_qz}} </span></p>
+                    <h4>敲诈客户</h4>
+                    <p><span> {{user_qz}} </span></p>
+                    <div class="icon fa fa-snowflake-o"></div>
                 </div>
             </el-col>
             <el-col :span="6">
                 <div class="col col-l">
-                    <p>欺诈团伙 <b>:</b> <span> {{user_group}} </span></p>
+                    <h4>欺诈团伙</h4>
+                    <p><span> {{user_group}} </span></p>
+                    <div class="icon fa fa-sign-language"></div>
                 </div>
             </el-col>
             <el-col :span="6">
                 <div class="col col-r">
-                    <p>待审核 <b>:</b> <span> {{num}} 客户</span></p>
+                    <h4>待审核</h4>
+                    <p><span> {{num}}</span></p>
+                    <div class="icon fa fa-superpowers"></div>
                 </div>
             </el-col>
         </el-row>
@@ -27,34 +34,61 @@
 </template>
 
 <script>
-export default {
-  name: 'card4',
-  data () {
-    return {
-      user: 800,
-      group: 8,
-      user_qz: 30,
-      user_group: 2,
-      num: 300
+  export default {
+    name: 'card4',
+    data () {
+      return {
+        user: 800,
+        group: 8,
+        user_qz: 30,
+        user_group: 21,
+        num: 300
+      }
     }
   }
-}
 </script>
 <style lang="scss" scoped>
-    @import '~@/assets/style/public.scss';
+    @import "~@/assets/style/public.scss";
+    *{
+        margin: 0;
+        padding: 0;
+    }
+    .fa {
+        margin-right: 20px;
+    }
+
     .col {
         padding: 20px;
         border-radius: 4px;
         border-width: 1px;
         border-style: solid;
         font-size: 16px;
+        transition: all 0.3s ease;
+        line-height: 26px;
+        position: relative;
+        &:hover{
+           transform: scale(1.05);
+        }
+        h4{
+            line-height: 32px;
+        }
+        .icon{
+            position: absolute;
+            right: 0;
+            top: 24px;
+            font-size: 42px;
+        }
+
+
     }
+
     .col-l {
-        background-color: lighten($color-info, 40%);
-        border-color: lighten($color-info, 35%);
+        background-color: rgba(175, 255, 177, 0.42);
+        border-color: rgba(208, 199, 205, 0.33);
     }
+
     .col-r {
-        background-color: lighten($color-success, 47%);
-        border-color: lighten($color-success, 40%);
+        background-color: rgba(82, 255, 241, 0.42);
+        border-color: rgba(208, 199, 205, 0.33);
     }
 </style>
