2294970a5c9976add18e05c62a8a9d0d9722a90d
lilish <xiangrikuifangao@sohu.com>
2018-09-13 09:27:34 +0800

    环境变量修改

--- src/libs/util.js
+++ src/libs/util.js
@@ -11,7 +11,7 @@ let util = {
  * @param {String} title 标题
  */
 util.title = function (titleText) {
-  const processTitle = process.env.VUE_APP_TITLE || 'D2Admin'
+  const processTitle = process.env.VUE_APP_TITLE || '金融风控云'
   window.document.title = `${processTitle}${titleText ? ` | ${titleText}` : ''}`
 }
 
