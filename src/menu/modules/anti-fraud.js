dcaf1c5ed8549d94a974ef73e6d687d347677098
frank <18380462011@163.com>
2018-09-14 15:44:34 +0800

    导航菜单的图标

--- src/menu/modules/anti-fraud.js
+++ src/menu/modules/anti-fraud.js
@@ -33,30 +33,30 @@ export default {
 }
 */
 export default [
-    { path: '/index', title: '首页', iconSvg: 'd2-crud' },
-    { path: '/antiFraud/addCustomer', title: '新客户提交', iconSvg: 'add_people' },
-    { path: '/antiFraud/examineCustomer', title: '审核管理', iconSvg: 'examine' },
+    { path: '/index', title: '首页', iconSvg: '首页' },
+    { path: '/antiFraud/addCustomer', title: '新客户提交', iconSvg: '新客户提交' },
+    { path: '/antiFraud/examineCustomer', title: '审核管理', iconSvg: '审核管理' },
     {
       title: '欺诈警告',
-      iconSvg: 'fraudAlert',
+      iconSvg: '欺诈报警',
       children: [
         { path: '/antiFraud/creditLoan', title: '信用贷客户',iconSvg: 'people' },
         { path: '/antiFraud/Mortgage', title: '抵押贷客户',iconSvg: 'people' },
         { path: '/antiFraud/XX', title: 'XX贷客户',iconSvg: 'people' }
       ]
     },
-    { path: '/antiFraud/searchCustomer', title: '客户检索', iconSvg: 'search_people' },
+    { path: '/antiFraud/searchCustomer', title: '客户检索', iconSvg: '客户检索' },
     {  
       title: '客户黑名单', 
-      iconSvg: 'blacklist',
+      iconSvg: '客户黑名单',
       children: [
-        { path: '/antiFraud/idCard', title: '身份证黑名单',iconSvg: 'idcard' },
-        { path: '/antiFraud/phone', title: '手机号黑名单',iconSvg: 'phone_black' },
-        { path: '/antiFraud/virtualAcount', title: '虚拟账号黑名单',iconSvg: 'virtualAcount' },
-        { path: '/antiFraud/ip', title: 'IP地址黑名单',iconSvg: 'ip' }
+        { path: '/antiFraud/idCard', title: '身份证黑名单',iconSvg: '身份证号黑名单' },
+        { path: '/antiFraud/phone', title: '手机号黑名单',iconSvg: '手机号黑名单' },
+        { path: '/antiFraud/virtualAcount', title: '虚拟账号黑名单',iconSvg: '虚拟账号黑名单' },
+        { path: '/antiFraud/ip', title: 'IP地址黑名单',iconSvg: 'ip地址黑名单' }
       ]
     },
-    { path: '/antiFraud/count', title: '统计报告', iconSvg: 'count' },
-    { path: '/antiFraud/manage', title: '业务管理', iconSvg: 'manage' },
+    { path: '/antiFraud/count', title: '统计报告', iconSvg: '统计报告' },
+    { path: '/antiFraud/manage', title: '业务管理', iconSvg: '业务管理' },
   ]
 
