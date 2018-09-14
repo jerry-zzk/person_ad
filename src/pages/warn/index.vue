ea9e4e249d5875fac36dd97ba3462e622ddf996c
zzk <zzk_lol@163.com>
2018-09-14 17:28:42 +0800

    警告 调整

--- src/pages/warn/index.vue
+++ src/pages/warn/index.vue
@@ -1,6 +1,5 @@
 <template>
-<div>
-  <d2-container class="page zk_page" >
+  <d2-container class="page zk_page"  >
     <!-- <template >数据模板</template> -->
     <el-header slot="header">
       <p>信贷种类 <b>:</b>&nbsp; <el-button size="mini" type="warning">全部</el-button><el-button class="bu" size="mini" type="primary">信用贷
@@ -28,45 +27,137 @@
         <el-input class="inp" size="mini"></el-input>-- &nbsp;&nbsp; <el-input class="inp" size="mini"></el-input>
       </p>
     </el-header>
-    <el-row>
-        <el-col :span="4" style="background:#fff;border-right:2px solid #ddd;">
-          <d2-customer-list :item="items1" style="height:800px;">
+    <div :min-percent='20' :default-percent='30' split="vertical">
+      <el-col slot="paneL" :span="4" style="background:#fff;border-right:2px solid #ddd;">
+        <d2-customer-list :item="items1" style="height:668px;">
             <!-- 数据编写 -->
-          </d2-customer-list>
-        </el-col>
-        <el-col :span="20" style="background:#fff;">
-        <p>
-          <!-- 表格 -->
-          <demo-page-header
-            slot="header"
-            @submit="handleSubmit"
-            ref="header"  @zk = "zk" style="padding-left:20px;margin-bottom:10px;border-bottom:1px solid #ddd;"/>
-          <demo-page-main style="padding-left:20px;"
-            :table-data="table"
-            :loading="loading" v-show="ok"/>
-        </p>
-        <el-col :span="24" v-show="!ok">
-            <div class="col col-l">
-              <el-row :gutter="23">
-                <el-col style="margin-bottom:10px;" :span="4" v-for="list in items" :key="list">
-                  <el-card shadow="hover" :body-style="{ padding: '0px' }">
-                    <!-- <img  @click="message()" v-bind:src="list.img" class="image" alt="图片"> -->
-                    <span class="cir">{{list.star}}</span> 
-                    <p class="lis1" :title="list.name">姓名 <b>:</b> <span style="font-size:15px;"> {{list.name}}</span></p>
-                    <p class="lis" :title="list.phone">手机号 <b>:</b><span style="font-size:15px;"> {{list.phone}}</span></p>
-                    <p class="lis" :title="list.peoNum">身份证号 <b>:</b><span style="font-size:15px;"> {{list.peoNum}}</span></p>
-                    <p class="lis" :title="list.type">种类 <b>:</b><span style="font-size:15px;"> {{list.type}}</span></p>
-                    <p class="lis" :title="list.time">时间 <b>:</b><span style="font-size:15px;"> {{list.time}}</span></p>
-                    <p class="lis">状态 <b>:</b> &nbsp;<el-button size="mini" class="midd1" :type="list.status"></el-button></p>
-                  </el-card>
-                </el-col>
-              </el-row>
-            </div>
+        </d2-customer-list>
+      </el-col>
+      <el-col slot="paneR" :span="20" style="background:#fff;">
+              <p>
+                <!-- 表格 -->
+                <demo-page-header
+                  slot="header"
+                  @submit="handleSubmit"
+                  ref="header"  @zk = "zk" style="padding-left:20px;margin-bottom:10px;border-bottom:1px solid #ddd;"/>
+                <demo-page-main style="padding-left:20px;"
+                  :table-data="table"
+                  :loading="loading" v-show="ok"/>
+              </p>
+              <el-col :span="24" v-show="!ok">
+                <div class="col col-l">
+                  <el-row :gutter="23">
+                    <el-col style="margin-bottom:10px;" :span="4" v-for="list in items" :key="list">
+                      <el-card shadow="hover" :body-style="{ padding: '0px' }">
+                        <!-- <img  @click="message()" v-bind:src="list.img" class="image" alt="图片"> -->
+                        <span class="cir" style="cursor:pointer;" @click="message">{{list.star}}</span> 
+                        <p class="lis1" :title="list.name">姓名 <b>:</b> <span style="font-size:15px;"> {{list.name}}</span></p>
+                        <p class="lis" :title="list.phone">手机号 <b>:</b><span style="font-size:15px;"> {{list.phone}}</span></p>
+                        <p class="lis" :title="list.peoNum">身份证号 <b>:</b><span style="font-size:15px;"> {{list.peoNum}}</span></p>
+                        <p class="lis" :title="list.type">种类 <b>:</b><span style="font-size:15px;"> {{list.type}}</span></p>
+                        <p class="lis" :title="list.time">时间 <b>:</b><span style="font-size:15px;"> {{list.time}}</span></p>
+                        <p class="lis">状态 <b>:</b> &nbsp;<el-button size="mini" class="midd1" :type="list.status"></el-button></p>
+                      </el-card>
+                    </el-col>
+                  </el-row>
+                </div>
+              </el-col>
           </el-col>
-        </el-col>
-    </el-row>
+      <!-- 全屏详细信息 -->
+      <el-dialog
+        :title="tooltipContent"
+        :fullscreen="true"
+        :visible.sync="dialogVisible"
+        :append-to-body="true">
+        <div class="d2-mb-10">
+          <el-card  class="d2-mb">
+            <el-row :gutter="10" style="margin-bottom:15px;">
+              <el-col :span="24">
+                <div class="col col-l">
+                  <el-button class="button-new-tag" size="small" @click="showInput">添加标签</el-button>
+                  <el-tag
+                  :key="index"
+                  v-for="tag,index in dynamicTags"
+                  closable
+                  :disable-transitions="false"
+                  @close="handleClose(tag)">
+                  {{tag}}
+                </el-tag>
+                <el-input
+                  class="input-new-tag"
+                  v-if="inputVisible"
+                  v-model="inputValue"
+                  ref="saveTagInput"
+                  size="small"
+                  @keyup.enter.native="handleInputConfirm"
+                  @blur="handleInputConfirm"
+                >
+                </el-input>
+                </div>
+              </el-col>
+            </el-row>
+            <p style="font-size:24px;">个人详细信息</p>
+            <el-row :gutter="10">
+              <el-col :span="5">
+                <div class="col col-d">
+                  <img src="../../assets/img/1.jpg" class="image1" alt="">
+                  <span style="display:inline-block;height:130px;width:170px;overflow-x:hidden;">
+                    <el-tag
+                      :key="tag"
+                      v-for="tag in dynamicTags"
+                      closable
+                      :disable-transitions="false"
+                      @close="handleClose(tag)">
+                      {{tag}}
+                    </el-tag>
+                  </span>
+                  <p>姓名 <b>:</b> {{zk_name}}</p>
+                  <p>电话 <b>:</b> {{zk_phone}}</p>
+                  <p>状态 <b>:</b> &nbsp;&nbsp;<el-button size="mini" class="midd1" type="danger"></el-button></p>
+                </div>
+              </el-col>
+              <el-col :span="18">
+                <div class="col col-d">
+                  （1）欺诈告警  对该企业新检查发现的欺诈客户向业务人员进行告警，由业务人员标记后从告警中移除。告警主要让业务人员对欺诈行为及时处理。
+                        其中告警内容需要多方面的展示欺诈检测不同指标内容。
+                        在审核的过程中，欺诈告警就已经在后台逐渐产生，产生一个告警一个。
+                        审核的结果报告单独页面展示，能够快速让业务人员发现问题，做出评判。<br>
+                  （2）欺诈告警  对该企业新检查发现的欺诈客户向业务人员进行告警，由业务人员标记后从告警中移除。告警主要让业务人员对欺诈行为及时处理。
+                        其中告警内容需要多方面的展示欺诈检测不同指标内容。
+                        在审核的过程中，欺诈告警就已经在后台逐渐产生，产生一个告警一个。
+                        审核的结果报告单独页面展示，能够快速让业务人员发现问题，做出评判。<br>
+                （3）欺诈告警  对该企业新检查发现的欺诈客户向业务人员进行告警，由业务人员标记后从告警中移除。告警主要让业务人员对欺诈行为及时处理。
+                        其中告警内容需要多方面的展示欺诈检测不同指标内容。
+                        在审核的过程中，欺诈告警就已经在后台逐渐产生，产生一个告警一个。
+                        审核的结果报告单独页面展示，能够快速让业务人员发现问题，做出评判。<br>
+                （4）欺诈告警  对该企业新检查发现的欺诈客户向业务人员进行告警，由业务人员标记后从告警中移除。告警主要让业务人员对欺诈行为及时处理。
+                        其中告警内容需要多方面的展示欺诈检测不同指标内容。
+                        在审核的过程中，欺诈告警就已经在后台逐渐产生，产生一个告警一个。
+                        审核的结果报告单独页面展示，能够快速让业务人员发现问题，做出评判。<br>
+                  （1）欺诈告警  对该企业新检查发现的欺诈客户向业务人员进行告警，由业务人员标记后从告警中移除。告警主要让业务人员对欺诈行为及时处理。
+                        其中告警内容需要多方面的展示欺诈检测不同指标内容。
+                        在审核的过程中，欺诈告警就已经在后台逐渐产生，产生一个告警一个。
+                        审核的结果报告单独页面展示，能够快速让业务人员发现问题，做出评判。<br>
+                  （2）欺诈告警  对该企业新检查发现的欺诈客户向业务人员进行告警，由业务人员标记后从告警中移除。告警主要让业务人员对欺诈行为及时处理。
+                        其中告警内容需要多方面的展示欺诈检测不同指标内容。
+                        在审核的过程中，欺诈告警就已经在后台逐渐产生，产生一个告警一个。
+                        审核的结果报告单独页面展示，能够快速让业务人员发现问题，做出评判。<br>
+                （3）欺诈告警  对该企业新检查发现的欺诈客户向业务人员进行告警，由业务人员标记后从告警中移除。告警主要让业务人员对欺诈行为及时处理。
+                        其中告警内容需要多方面的展示欺诈检测不同指标内容。
+                        在审核的过程中，欺诈告警就已经在后台逐渐产生，产生一个告警一个。
+                        审核的结果报告单独页面展示，能够快速让业务人员发现问题，做出评判。<br>
+                  （4）欺诈告警  对该企业新检查发现的欺诈客户向业务人员进行告警，由业务人员标记后从告警中移除。告警主要让业务人员对欺诈行为及时处理。
+                        其中告警内容需要多方面的展示欺诈检测不同指标内容。
+                        在审核的过程中，欺诈告警就已经在后台逐渐产生，产生一个告警一个。
+                        审核的结果报告单独页面展示，能够快速让业务人员发现问题，做出评判。<br>
+                </div>
+              </el-col>
+            </el-row>
+          </el-card>
+        </div>
+      </el-dialog>
+    </div>
   </d2-container>
-</div>
 </template>
 <script>
   import { BusinessTable1List } from '@/api/demo/business/table/1'
@@ -82,6 +173,24 @@
     },
     data () {
       return {
+        zk_name: 'zk',
+        zk_phone: '15522322212',
+        item: [
+          { name: 'zk3', status: 'danger' ,num:101},
+          { name: 'zk1', status: 'success' ,num:101},
+          { name: 'zk2', status: 'warning',num:101 },
+          { name: 'zk2', status: 'warning' ,num:101},
+          { name: 'zk2', status: 'success' ,num:101},
+          { name: 'zk2', status: 'danger' ,num:101},
+          { name: 'zk2', status: 'warning' ,num:101},
+          { name: 'zk2', status: 'success' ,num:101},
+          { name: 'zk2', status: 'warning' ,num:1019},
+          { name: 'zk2', status: 'danger' ,num:103}
+        ],
+        dialogVisible:false,
+        dynamicTags: [],
+        inputVisible: false,
+        inputValue: '',
         param:'2018-2-10',
         param2:'2018-2-16',
         param3:'2018-1-10',
@@ -120,6 +229,7 @@
     },
     methods: {
       message () {
+        // alert(123213)
         this.dialogVisible = true
       },
       zk (msg) {
@@ -130,6 +240,23 @@
           this.ok1 = false
           this.ok = true
         }
+      },
+      handleClose (tag) {
+        this.dynamicTags.splice(this.dynamicTags.indexOf(tag), 1)
+      },
+      showInput () {
+        this.inputVisible = true
+        this.$nextTick(_ => {
+          this.$refs.saveTagInput.$refs.input.focus()
+        })
+      },
+      handleInputConfirm () {
+        let inputValue = this.inputValue
+        if (inputValue) {
+          this.dynamicTags.push(inputValue)
+        }
+        this.inputVisible = false
+        this.inputValue = ''
       },
       handlePaginationChange (val) {
         this.$notify({
