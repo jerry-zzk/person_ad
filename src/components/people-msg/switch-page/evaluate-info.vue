57dc98625388637fc9eba58f9c581bb4b416e364
1-PC\1 <xiangrikuifangao@sohu.com>
2018-09-17 22:45:46 +0800

    no message

--- src/components/people-msg/switch-page/evaluate-info.vue
+++ src/components/people-msg/switch-page/evaluate-info.vue
@@ -3,7 +3,7 @@
 		<div id="evaluete-info-header">
 			<ul>
 			  <li :index="item.index" v-for="item in menu_item">
-			  	<a href="javascript:void(0)" @click="goAnchor('#anchor-'+item.index)" target="_blank">
+			  	<a :class="{'active':item.select}"  href="javascript:void(0)" @click="goAnchor(item)" target="_blank">
 			  		{{item.name}}
 			  	</a>
 			  	<span v-if="item.index!=6" class="bar">
@@ -16,237 +16,216 @@
 		</div>
 		<div id="evaluete-info-main">
 			<el-scrollbar style="height: 100%">
-				<div id="anchor-1" class="section">
-					
-				</div>
-				<div id="anchor-2" class="section">
-					
-				</div>
-				<!--设备欺诈 -->
-				<div id="anchor-3" class="section">
-					<p class="title">设备欺诈</p>
-					<p>设备评分：<span class="red"><i class="fa fa-waring"></i>有风险</span></p>
-					<div class="echart" style="height:500px;width:500px" ref="myEchart1"></div>
-					<div class="guan_lian_hao">
-						<p><i class="fa fa-waring"></i>有风险</p>
-						<ul class="waring-list">
-							<li class="list-item">139*******</li>
-							<li class="list-item">139*******</li>
-							<li class="list-item">139*******</li>
-							<li class="list-item">139*******</li>
-						</ul>
+				<div id="evaluete-info-main-inner">
+					<div id="anchor-1" class="section">
+						
 					</div>
-					<div class="guan_lian_hao">
-						<p><i class="fa fa-waring"></i>疑似黑中介wifi</p>
-						<ul class="waring-list">
-							<li class="list-item">网络环境</li>
-						</ul>
+					<div id="anchor-2" class="section">
+						
 					</div>
-					<div class="wei_zhi_xin_xi">
-						<p><i class="fa fa-waring"></i>疑似黑中介地址</p>
-						<ul class="waring-list">
-							<li class="list-item">位置信息</li>
-						</ul>
+					<!--设备欺诈 -->
+					<div id="anchor-3" class="section">
+						<p class="title">设备欺诈</p>
+						<p class="main-title">设备评分：<span class="red"><i class="fa fa-warning"></i> 有风险</span></p>
+						<div class="echarts" ref="myEchart1"></div>
+						<div class="container guan_lian_hao">
+							<p><i class="fa fa-warning"></i>有风险</p>
+							<ul class="waring-list">
+								<li class="list-item">139*******</li>
+								<li class="list-item">139*******</li>
+								<li class="list-item">139*******</li>
+								<li class="list-item">139*******</li>
+							</ul>
+						</div>
+						<div class="container wang_luo_huan_jing">
+							<p><i class="fa fa-warning"></i>疑似黑中介wifi</p>
+							<ul class="waring-list">
+								<li class="list-item">网络环境</li>
+							</ul>
+						</div>
+						<div class="container wei_zhi_xin_xi">
+							<p><i class="fa fa-warning"></i>疑似黑中介地址</p>
+							<ul class="waring-list">
+								<li class="list-item">位置信息</li>
+							</ul>
+						</div>
+						<div class="container IP_yi_chang">
+							<p><i class="fa fa-warning"></i>疑似团伙欺诈</p>
+							<ul class="waring-list">
+								<li class="list-item">ip地址</li>
+							</ul>
+							<p><i class="fa fa-warning"></i>黑中介风险</p>
+							<ul class="waring-list">
+								<li class="list-item">ip地址</li>
+							</ul>
+						</div>
+						<div class="container xu_ni_hao_ma">
+							<table>
+								<tr>
+									<td class="first color1">微博</td>
+									<td>**********</td>
+								</tr>
+								<tr>
+									<td class="first color2">微信</td>
+									<td>**********</td>
+								</tr>
+							</table>
+						</div>
+						<div class="container xing_hao_xi_tong">
+							<table>
+								<tr>
+									<td class="first color1">系统</td>
+									<td>**********</td>
+								</tr>
+								<tr>
+									<td class="first color2">账号</td>
+									<td>**********</td>
+								</tr>
+							</table>
+						</div>
 					</div>
-					<div class="IP_yi_chang">
-						<p><i class="fa fa-waring"></i>疑似团伙欺诈</p>
-						<ul class="waring-list">
-							<li class="list-item">ip地址</li>
-						</ul>
-						<p><i class="fa fa-waring"></i>黑中介风险</p>
-						<ul class="waring-list">
-							<li class="list-item">ip地址</li>
-						</ul>
-					</div>
-					<div class="xu_ni_hao_ma">
-						<table>
-							<tr>
-								<td class="first color1">
-									微博
-								</td>
-								<td>
-									**********
-								</td>
-							</tr>
-							<tr>
-								<td class="first color2">
-									微信
-								</td>
-								<td>
-									**********
-								</td>
-							</tr>
-						</table>
-					</div>
-					<div class="xing_hao_xi_tong">
-						<table>
-							<tr>
-								<td class="first color1">
-									系统
-								</td>
-								<td>
-									**********
-								</td>
-							</tr>
-							<tr>
-								<td class="first color2">
-									账号
-								</td>
-								<td>
-									**********
-								</td>
-							</tr>
-						</table>
-					</div>
-				</div>
-				<!--团伙关联 -->
-				<div id="anchor-4" class="section">
-					<el-row>
-						<el-col :span="12">
-							<p class="title">通联关系</p>
-							<div class="echart" style="width:100%;height:500px" ref="myEchart2"></div>
-							<div class="table">
-								<p>详情</p>
-								<table>
-									<tr>
-										<td class="first color1">
-											系统
-										</td>
-										<td>
-											**********
-										</td>
-									</tr>
-									<tr>
-										<td class="first color2">
-											账号
-										</td>
-										<td>
-											**********
-										</td>
-									</tr>
-								</table>
-							</div>
-						</el-col>
-						<el-col :span="12">
-							<p class="title">虚拟身份</p>
-							<div class="echart" style="width:100%;height:500px" ref="myEchart3"></div>
-							<div class="table">
-								<p>详情</p>
-								<table>
-									<tr>
-										<td class="first color1">
-											系统
-										</td>
-										<td>
-											**********
-										</td>
-									</tr>
-									<tr>
-										<td class="first color2">
-											账号
-										</td>
-										<td>
-											**********
-										</td>
-									</tr>
-								</table>
-							</div>
-						</el-col>
-						<el-col :span="24">
-							<p class="title">行为关联风险</p>
-							<div class="table">
-								<p>详情</p>
-								<table>
-									<tr>
-										<td class="first color1">
-											系统
-										</td>
-										<td>
-											**********
-										</td>
-									</tr>
-									<tr>
-										<td class="first color2">
-											账号
-										</td>
-										<td>
-											**********
-										</td>
-									</tr>
-								</table>
-							</div>
-						</el-col>
-					</el-row>
-				</div>
-				<!--金融属性 -->
-				<div id="anchor-5" class="section">
-					<el-row>
-						<el-col :span="12">
-							<el-col :span="24">
-								<p class="title">催收电话</p>
-								<table>
-									<tr>
-										<td class="first color1">138********</td>
-										<td>2018年1月3日</td>
-										<td>建设银行</td>
-									</tr>
-									<tr>
-										<td class="first color2">138********</td>
-										<td>2018年1月3日</td>
-										<td>建设银行</td>
-									</tr>
-								</table>
+					<!--团伙关联 -->
+					<div id="anchor-4" class="section">
+						<el-row>
+							<el-col :span="12">
+								<p class="title">通联关系</p>
+								<div class="echart" ref="myEchart2"></div>
+								<div class="table">
+									<p>详情</p>
+									<table>
+										<tr>
+											<td class="first color1">张三</td>
+											<td>黑名单</td>
+											<td>日期</td>
+										</tr>
+										<tr>
+											<td class="first color2">李四</td>
+											<td>黑名单</td>
+											<td>日期</td>
+										</tr>
+									</table>
+								</div>
+							</el-col>
+							<el-col :span="12">
+								<p class="title">虚拟身份</p>
+								<div class="echart" ref="myEchart3"></div>
+								<div class="table">
+									<p>详情</p>
+									<table>
+										<tr>
+											<td class="first color1">张三/td>
+											<td>黑名单</td>
+											<td>日期</td>
+											<td>微博转发</td>
+										</tr>
+										<tr>
+											<td class="first color2">李四</td>
+											<td>黑名单</td>
+											<td>日期</td>
+											<td>微博转发</td>
+										</tr>
+									</table>
+								</div>
 							</el-col>
 							<el-col :span="24">
-								<p class="title">催收短信</p>
-								<table>
-									<tr>
-										<td class="first color1">138********</td>
-										<td>2018年1月3日</td>
-										<td>建设银行</td>
-									</tr>
-									<tr>
-										<td class="first color2">138********</td>
-										<td>2018年1月3日</td>
-										<td>建设银行</td>
-									</tr>
-								</table>
+								<p class="title">行为关联风险</p>
+								<div class="table">
+									<table>
+										<tr>
+											<td class="first color1">多个设备同时申请</td>
+											<td>时间</td>
+											<td>设备</td>
+										</tr>
+										<tr>
+											<td class="first color2">多个设备同IP申请</td>
+											<td>IP</td>
+											<td>设备</td>
+										</tr>
+										<tr>
+											<td class="first color3">多个设备同位置P申请</td>
+											<td>位置</td>
+											<td>设备</td>
+										</tr>
+										<tr>
+											<td class="first color4">多个身份同设备申请</td>
+											<td>设备</td>
+											<td>身份</td>
+										</tr>
+									</table>
+								</div>
 							</el-col>
-						</el-col>
-						<el-col :span="12">
-							<p class="title">高危消费行为</p>
-							<div class="echart" style="width:100%;height:500px" ref="myEchart4"></div>
-						</el-col>
-						<el-col :span="12">
-							<p class="title">贷款类app注册</p>
-							<div class="echart" style="width:100%;height:500px" ref="myEchart5"></div>
-						</el-col>
-						<el-col :span="12">
-							<p class="title">高危消费类型</p>
-							<div class="echart" style="width:100%;height:500px" ref="myEchart6"></div>
-						</el-col>
-					</el-row>
-				</div>
-				<!--网络行为 -->
-				<div id="anchor-6" class="section">
-					<el-row>
-						<el-col :span="12">
-							<p class="title">APP使用</p>
-							<div class="echart" style="width:100%;height:500px" ref="myEchart7"></div>
-						</el-col>
-						<el-col :span="12">
-							<p class="title">社交行为</p>
-							<div class="echart" style="width:100%;height:500px" ref="myEchart8"></div>
-						</el-col>
-						<el-col :span="12">
-							<p class="title">浏览行为</p>
-							<div class="echart" style="width:100%;height:500px" ref="myEchart9"></div>
-						</el-col>
-						<el-col :span="12">
-							<p class="title">搜索行为</p>
-							<div class="echart" style="width:100%;height:500px" ref="myEchart10"></div>
-						</el-col>
-					</el-row>
+						</el-row>
+					</div>
+					<!--金融属性 -->
+					<div id="anchor-5" class="section">
+						<el-row>
+							<el-col :span="12">
+								<el-col :span="24">
+									<p class="title">催收电话</p>
+									<table>
+										<tr>
+											<td class="first color1">138********</td>
+											<td>2018年1月3日</td>
+											<td>建设银行</td>
+										</tr>
+										<tr>
+											<td class="first color2">138********</td>
+											<td>2018年1月3日</td>
+											<td>建设银行</td>
+										</tr>
+									</table>
+								</el-col>
+								<el-col :span="24">
+									<p class="title">催收短信</p>
+									<table>
+										<tr>
+											<td class="first color1">138********</td>
+											<td>2018年1月3日</td>
+											<td>建设银行</td>
+										</tr>
+										<tr>
+											<td class="first color2">138********</td>
+											<td>2018年1月3日</td>
+											<td>建设银行</td>
+										</tr>
+									</table>
+								</el-col>
+							</el-col>
+							<el-col :span="12">
+								<p class="title">高危消费行为</p>
+								<div class="echart"  ref="myEchart4"></div>
+							</el-col>
+							<el-col :span="12">
+								<p class="title">贷款类app注册</p>
+								<div class="echart"  ref="myEchart5"></div>
+							</el-col>
+							<el-col :span="12">
+								<p class="title">高危消费类型</p>
+								<div class="echart"  ref="myEchart6"></div>
+							</el-col>
+						</el-row>
+					</div>
+					<!--网络行为 -->
+					<div id="anchor-6" class="section">
+						<el-row>
+							<el-col :span="12">
+								<p class="title">APP使用</p>
+								<div class="echart"  ref="myEchart7"></div>
+							</el-col>
+							<el-col :span="12">
+								<p class="title">社交行为</p>
+								<div class="echart"  ref="myEchart8"></div>
+							</el-col>
+							<el-col :span="12">
+								<p class="title">浏览行为</p>
+								<div class="echart"  ref="myEchart9"></div>
+							</el-col>
+							<el-col :span="12">
+								<p class="title">搜索行为</p>
+								<div class="echart"  ref="myEchart10"></div>
+							</el-col>
+						</el-row>
+					</div>
 				</div>
 			</el-scrollbar>
 		</div>
@@ -262,22 +241,28 @@ export default {
     return {
     	menu_item:[{
     		index:1,
-    		name:"身份核验"
+    		name:"身份核验",
+    		select:true
     	},{
     		index:2,
-    		name:"黑名单"
+    		name:"黑名单",
+    		select:false
     	},{
     		index:3,
-    		name:"设备欺诈"
+    		name:"设备欺诈",
+    		select:false
     	},{
     		index:4,
-    		name:"团伙关联"
+    		name:"团伙关联",
+    		select:false
     	},{
     		index:5,
-    		name:"金融属性"
+    		name:"金融属性",
+    		select:false
     	},{
     		index:6,
-    		name:"网络行为"
+    		name:"网络行为",
+    		select:false
     	}],
     	chart1: null,
     	chart2: null,
@@ -321,20 +306,25 @@ export default {
     this.chart6 = this.chart7 = this.chart8 = this.chart9 = this.chart10=null;
   },
   methods:{
-  	goAnchor(selector) {
-        var anchor = this.$el.querySelector(selector)
-        document.documentElement.scrollTop = anchor.offsetTop
+  	goAnchor(item) {
+  		this.menu_item.forEach((item, index) => {
+  			item.select = false;
+		});
+  		item.select = true;
+        var anchor = this.$el.querySelector('#anchor-'+item.index);
+       
+        document.querySelector(".el-scrollbar__view").scrollTop = anchor.offsetTop
     },
     initChart1() {
       	this.chart1 = echarts.init(this.$refs.myEchart1);
       	// 把配置和数据放这里
       	this.chart1.setOption({
 		    title: {
-		        text: '基础雷达图'
+		        text: ''
 		    },
 		    tooltip: {},
 		    legend: {
-		        data: ['预算分配（Allocated Budget）', '实际开销（Actual Spending）']
+		        show:false
 		    },
 		    radar: {
 		        // shape: 'circle',
@@ -363,10 +353,6 @@ export default {
 		            {
 		                value : [4300, 10000, 28000, 35000, 50000, 19000],
 		                name : '预算分配（Allocated Budget）'
-		            },
-		             {
-		                value : [5000, 14000, 28000, 31000, 42000, 21000],
-		                name : '实际开销（Actual Spending）'
 		            }
 		        ]
 		    }]
