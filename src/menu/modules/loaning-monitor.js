9afdd0ef3c0ca89cdf359265ffd38b4673006180
frank <18380462011@163.com>
2018-09-18 15:55:15 +0800

    部分菜单图标

--- src/menu/modules/loaning-monitor.js
+++ src/menu/modules/loaning-monitor.js
@@ -1,11 +1,11 @@
 export default [
     { path: '/loaningMonitor/consumerMgt', title: '客户管理', iconSvg: 'ihome' },
-    { path: '/loaningMonitor/situationWarning', title: '态势预警', iconSvg: 'inewConsumerSubmit' },
-    { path: '/loaningMonitor/riskAssessment', title: '客户风险评估', iconSvg: 'iauditsMgt' },
-    { path: '/loaningMonitor/lossRepair', title: '失联修复', iconSvg: 'icheatWarn' },
-    { path: '/loaningMonitor/blackList', title: '客户黑名单', iconSvg: 'iconsumerSearch' },
-    { path: '/loaningMonitor/rulesMgt', title: '规则集管理', iconSvg: 'istatistical' },
-    { path: '/loaningMonitor/statisticReport', title: '统计报告', iconSvg: 'iBusinessMgt' },
+    { path: '/loaningMonitor/situationWarning', title: '态势预警', iconSvg: 'icheatWarn' },
+    { path: '/loaningMonitor/riskAssessment', title: '客户风险评估', iconSvg: 'iconsumerSearch' },
+    { path: '/loaningMonitor/lossRepair', title: '失联修复', iconSvg: 'iauditsMgt' },
+    { path: '/loaningMonitor/blackList', title: '客户黑名单', iconSvg: 'iconsumerBlacklist' },
+    { path: '/loaningMonitor/rulesMgt', title: '规则集管理', iconSvg: 'inewConsumerSubmit' },
+    { path: '/loaningMonitor/statisticReport', title: '统计报告', iconSvg: 'istatistical' },
     { path: '/loaningMonitor/businessMgt', title: '业务管理', iconSvg: 'iBusinessMgt' }
   ]
 
