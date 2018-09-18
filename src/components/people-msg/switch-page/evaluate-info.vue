c8605fc4b4d3208145ebf5c9fc8de548c6b45d65
lilish <xiangrikuifangao@sohu.com>
2018-09-18 11:04:19 +0800

    no message

--- src/components/people-msg/switch-page/evaluate-info.vue
+++ src/components/people-msg/switch-page/evaluate-info.vue
@@ -1500,10 +1500,7 @@ export default {
 		    ]
 		})
     },
-<<<<<<< HEAD
-=======
     // 热力图
->>>>>>> 09d23dbe87940f91e6828d52ed0d5c4c65e8079f
     drawHeatMap(){
       let _this = this;
       let option = {
