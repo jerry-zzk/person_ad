101099ed56f4e35f83ff9945683cdaf862410bc1
lilish <xiangrikuifangao@sohu.com>
2018-09-19 16:09:43 +0800

    添加组件 d2-curd https://github.com/d2-projects/d2-crud

--- src/components/people-msg1/switch-page/first.vue
+++ src/components/people-msg1/switch-page/first.vue
@@ -1,18 +1,22 @@
 <template>
-	<el-scrollbar style="height: 100%">
+	<el-scrollbar style="height: 100%" v-resize:throttle="onResize">
 		<el-container>
 			<el-main>
 				<el-row>
 					<el-col :span="24">
 						<div class="container">
 							<p class="title">贷中风险评分</p>
-							<div class="text">风险评分：<span>90</span></div>
+							<div class="title-text">风险评分：<span>90</span></div>
 							<div class="chart" ref="chart"></div>	
 						</div>
 					</el-col>
 					<el-col :span="24">
 						<div class="container">
-							<table class="col-table">
+							 <d2-crud
+						      ref="d2Crud"
+						      :columns="columns"
+						      :data="data"/>
+							<!-- <table class="col-table">
 								<th>
 									<td>规则集</td>
 									<td>规则</td>
@@ -80,7 +84,7 @@
 									<td>大额消费记录（固定资产类/金融类/消费类）</td>
 									<td>8</td>
 								</tr>
-							</table>
+							</table> -->
 						</div>
 					</el-col>
 				</el-row>
@@ -90,6 +94,16 @@
 </template>
 <style lang="scss" scoped>
 @import '~@/assets/style/unit/info.scss';
+.title-text{
+	position: absolute;
+	top:70px;
+	left:40px;
+	font-size:24px;
+	span{
+		font-size:40px;
+		color:red;
+	}
+}
 </style>
 <script>
 import echarts from 'echarts'
@@ -97,16 +111,100 @@ import resize from 'vue-resize-directive'
 export default {
   data () {
     return {
-    	
+    	chart: null,
+    	columns: [
+	        {
+	          title: '日期',
+	          key: 'date',
+	          width: '180'
+	        },
+	        {
+	          title: '姓名',
+	          key: 'name',
+	          width: '180'
+	        },
+	        {
+	          title: '地址',
+	          key: 'address'
+	        }
+	      ],
+      	data: [
+	        {
+	          date: '2016-05-02',
+	          name: '王小虎',
+	          address: '上海市普陀区金沙江路 1518 弄'
+	        },
+	        {
+	          date: '2016-05-04',
+	          name: '王小虎',
+	          address: '上海市普陀区金沙江路 1517 弄'
+	        },
+	        {
+	          date: '2016-05-01',
+	          name: '王小虎',
+	          address: '上海市普陀区金沙江路 1519 弄'
+	        },
+	        {
+	          date: '2016-05-03',
+	          name: '王小虎',
+	          address: '上海市普陀区金沙江路 1516 弄'
+	        }
+        ]
    	}
+  },
+  mounted() {
+    this.initChart();
+  },
+  beforeDestroy() {
+  	this.chart.dispose();
+  	this.chart= null;
   },
   directives: {
     resize
   },
   methods:{
   	onResize:function(){
-  		
-  	}
+  		this.chart.resize();
+  	},
+  	initChart() {
+      	this.chart = echarts.init(this.$refs.chart);
+      	this.chart.setOption({
+		    title: {
+		        text: ""
+		    },
+		    tooltip: {
+		        trigger: 'axis'
+		    },
+		    radar: [
+		        {
+		            indicator: [
+		                {text: '失联倾向异常', max: 100},
+		                {text: '关联异常', max: 100},
+		                {text: '位置异常', max: 100},
+		                {text: '网络行为异常', max: 100}
+		            ],
+		            radius: 80
+		        },
+		     
+		    ],
+		    series: [
+		        {
+		            type: 'radar',
+		             tooltip: {
+		                trigger: 'item'
+		            },
+		            itemStyle: {normal: {areaStyle: {type: 'default'}}},
+		            data: [
+		                {
+		                    value: [66,78,95,90],
+		                    name: 'XXX'
+		                }
+		            ]
+		        },
+		      
+		    ]
+		})
+    }
   }
  }
 </script>
