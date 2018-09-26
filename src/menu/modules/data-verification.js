52e8dab9ea460b90e3a1bd162ccdfb718acac283
frank <18380462011@163.com>
2018-09-26 14:51:03 +0800

    第三个模块导航太长

--- src/menu/modules/data-verification.js
+++ src/menu/modules/data-verification.js
@@ -1,3 +1,4 @@
+/*
 export default [
   { title: '身份核验', iconSvg: 'ihome',
     children: [
@@ -62,4 +63,68 @@ export default [
     ]
   }
 ]
-
+*/
+export default [
+  { title: '身份核验', iconSvg: 'ihome',
+    children: [
+        { path: '/dataVerification/namePhone', title: '姓名/手机号',iconSvg: 'iidCardBlacklist' },
+        { path: '/dataVerification/idPhone', title: '身份证号/手机号',iconSvg: 'itelephoneBlacklist' },
+        { path: '/dataVerification/sexPhone', title: '性别/手机号',iconSvg: 'ivirtualAccountBlacklist' },
+        { path: '/dataVerification/peoPhone', title: '手机号/使用人职业',iconSvg: 'iipAdressBlacklist' },
+        { path: '/dataVerification/addPhone', title: '手机号/所在单位',iconSvg: 'iipAdressBlacklist' },
+    ]
+  },
+  { path: '/dataVerification/addCustomer', title: '虚拟帐号核验', iconSvg: 'inewConsumerSubmit',
+    children: [
+        { path: '/dataVerification/nameEmail', title: '姓名/邮箱',iconSvg: 'iidCardBlacklist' },
+        { path: '/dataVerification/nameEmail1', title: '企业/邮箱',iconSvg: 'ivirtualAccountBlacklist' },
+        { path: '/dataVerification/nameEmail2', title: '虚拟号码个数',iconSvg: 'iidCardBlacklist' },
+        { path: '/dataVerification/nameEmail3', title: '手机号/虚拟账号',iconSvg: 'itelephoneBlacklist' },
+        { path: '/dataVerification/nameEmail4', title: '手机号/信用卡',iconSvg: 'iipAdressBlacklist' },
+        { path: '/dataVerification/nameEmail5', title: '手机号/虚拟身份',iconSvg: 'itelephoneBlacklist' },
+        { path: '/dataVerification/nameEmail6', title: '贷款类APP个数',iconSvg: 'iidCardBlacklist' },
+        { path: '/dataVerification/nameEmail7', title: '手机号/IMSI',iconSvg: 'itelephoneBlacklist' },
+    ]
+  },
+  { path: '/dataVerification/examineCustomer', title: '设备核验', iconSvg: 'iauditsMgt',
+    children: [
+        { path: '/dataVerification/naph', title: '同设备多手机号',iconSvg: 'iidCardBlacklist' },
+        { path: '/dataVerification/naph1', title: '设备操作系统',iconSvg: 'itelephoneBlacklist' },
+        { path: '/dataVerification/naph2', title: '设备型号',iconSvg: 'ivirtualAccountBlacklist' },
+        { path: '/dataVerification/naph3', title: '同手机号设备数量',iconSvg: 'iipAdressBlacklist' },
+        { path: '/dataVerification/naph4', title: '手机号与IMEI关系',iconSvg: 'iipAdressBlacklist' },
+    ]
+  },
+  { path: '/dataVerification/creditLoan', title: '位置核验', iconSvg: 'icheatWarn',
+      children: [
+        { path: '/dataVerification/place', title: '手机号归属地',iconSvg: 'iidCardBlacklist' },
+        { path: '/dataVerification/place1', title: '当前所在省市区县',iconSvg: 'itelephoneBlacklist' },
+        { path: '/dataVerification/place2', title: '近期活跃省份和城市',iconSvg: 'ivirtualAccountBlacklist' },
+        { path: '/dataVerification/place3', title: '居住地',iconSvg: 'iipAdressBlacklist' },
+        { path: '/dataVerification/place4', title: '工作地',iconSvg: 'itelephoneBlacklist' },
+        { path: '/dataVerification/place5', title: '机主籍贯',iconSvg: 'iipAdressBlacklist' },
+        { path: '/dataVerification/place6', title: '历史时间点出现位置',iconSvg: 'iidCardBlacklist' },
+        { path: '/dataVerification/place7', title: '在某地活跃时间',iconSvg: 'ivirtualAccountBlacklist' },
+        { path: '/dataVerification/place8', title: '两个手机号位置共现',iconSvg: 'iipAdressBlacklist' },
+        { path: '/dataVerification/place9', title: '出行规律',iconSvg: 'ivirtualAccountBlacklist' },
+    ]
+  },
+  { path: '/dataVerification/searchCustomer', title: '关系核验', iconSvg: 'iconsumerSearch',
+    children: [
+        { path: '/dataVerification/relation', title: '两个手机号熟悉程度',iconSvg: 'iidCardBlacklist' },
+        { path: '/dataVerification/relation1', title: '两个手机号社会关系',iconSvg: 'itelephoneBlacklist' },
+        { path: '/dataVerification/relation2', title: '两个虚拟账号的关系',iconSvg: 'ivirtualAccountBlacklist' },
+        { path: '/dataVerification/relation3', title: '多个实体的群体关系',iconSvg: 'iipAdressBlacklist' },
+        { path: '/dataVerification/relation4', title: '两个手机号通联频次',iconSvg: 'iipAdressBlacklist' },
+    ]
+  },
+  { path: '/dataVerification/manage', title: '行为核验', iconSvg: 'iBusinessMgt',
+    children: [
+        { path: '/dataVerification/action', title: '车牌违章行为',iconSvg: 'iidCardBlacklist' },
+        { path: '/dataVerification/action1', title: '搜索词',iconSvg: 'itelephoneBlacklist' },
+        { path: '/dataVerification/action2', title: '近期搜索热词',iconSvg: 'ivirtualAccountBlacklist' },
+        { path: '/dataVerification/action3', title: '借贷行为',iconSvg: 'iipAdressBlacklist' },
+        { path: '/dataVerification/action4', title: '收入和月消费水平',iconSvg: 'iipAdressBlacklist' },
+    ]
+  }
+]
