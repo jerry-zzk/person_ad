33d6586f74d813b249bb078b0976817faa30c09a
gcx <970659339@qq.com>
2018-09-27 15:41:27 +0800

    no message

--- src/components/people-msg1/switch-page/first.vue
+++ src/components/people-msg1/switch-page/first.vue
@@ -15,69 +15,69 @@
 							<table class="col-table">
 								<tr>
 									<th>规则集</th>
-									<th>规则</th>
+									<th class="table_td2">规则</th>
 									<th>权重</th>
 									<th>评分</th>
 									<th>详情</th>
 								</tr>
 								<tr>
 									<td class="bg-gray blue" rowspan="3">失联倾向</td>
-									<td>预留手机持续关机</td>
+									<td class="table_td2">预留手机持续关机</td>
 									<td class="f-orange" >8</td>
 									<td class="f-red" rowspan="3">90</td>
 									<td rowspan="3" @click="tabChange(2)"><i class="fa fa-info f-blue"></i> 详情查看</td>
 								</tr>
 								<tr>
-									<td>出省，出境</td>
+									<td class="table_td2">出省，出境</td>
 									<td class="f-orange" >8</td>
 								</tr>
 								<tr>
-									<td>更换新手机号</td>
+									<td class="table_td2">更换新手机号</td>
 									<td class="f-orange" >8</td>
 								</tr>
 								<tr>
 									<td class="bg-gray orange" rowspan="3">关系异常</td>
-									<td>身份关联异常</td>
+									<td class="table_td2">身份关联异常</td>
 									<td class="f-orange" >8</td>
 									<td class="f-red" rowspan="3">90</td>
 									<td rowspan="3" @click="tabChange(3)"><i class="fa fa-info f-blue"></i> 详情查看</td>
 								</tr>
 								<tr>
-									<td>群体关联异常</td>
+									<td class="table_td2">群体关联异常</td>
 									<td class="f-orange" >8</td>
 								</tr>
 								<tr>
-									<td>身份关联异常</td>
+									<td class="table_td2">身份关联异常</td>
 									<td class="f-orange"  >8</td>
 								</tr>
 								<tr>
 									<td class="bg-gray blue1" rowspan="3">位置异常</td>
-									<td>频繁出现高档消费场所</td>
+									<td class="table_td2">频繁出现高档消费场所</td>
 									<td class="f-orange" >8</td>
 									<td class="f-green" rowspan="3">40</td>
 									<td rowspan="3" @click="tabChange(4)"><i class="fa fa-info f-blue"></i> 详情查看</td>
 								</tr>
 								<tr>
-									<td>频繁出省出境</td>
+									<td class="table_td2">频繁出省出境</td>
 									<td class="f-orange" >8</td>
 								</tr>
 								<tr>
-									<td>重要位置和贷前差别较大（居住场所更新）</td>
+									<td class="table_td2">重要位置和贷前差别较大（居住场所更新）</td>
 									<td class="f-orange" >8</td>
 								</tr>
 								<tr>
 									<td class="bg-gray purple" rowspan="3">行为异常</td>
-									<td>网络应用异常</td>
+									<td class="table_td2">网络应用异常</td>
 									<td class="f-orange">8</td>
 									<td class="f-red" rowspan="3">90</td>
 									<td rowspan="3" @click="tabChange(5)"><i class="fa fa-info f-blue"></i> 详情查看</td>
 								</tr>
 								<tr>
-									<td>网络浏览/发布/搜索等行为异常</td>
+									<td class="table_td2">网络浏览/发布/搜索等行为异常</td>
 									<td class="f-orange" >8</td>
 								</tr>
 								<tr>
-									<td>大额消费记录（固定资产类/金融类/消费类）</td>
+									<td class="table_td2">大额消费记录（固定资产类/金融类/消费类）</td>
 									<td class="f-orange" >8</td>
 								</tr>
 							</table>
@@ -100,6 +100,11 @@
 		color:red;
 	}
 }
+	.table_td2{
+		text-align: left!important;
+		padding-left: 60px!important;
+		border-right: none!important;
+	}
 </style>
 <script>
 import echarts from 'echarts'
