6694335b1cf6ff359a43092c38b7da4cf29bb3b3
frank <18380462011@163.com>
2018-09-27 16:21:23 +0800

    样图标大小微调

--- src/layout/header-aside/components/header-gohome/index.vue
+++ src/layout/header-aside/components/header-gohome/index.vue
@@ -2,7 +2,7 @@
   <div>
     <el-tooltip effect="dark" content="回到导览页" placement="bottom">
       <el-button class="d2-ml-0 d2-mr btn-text can-hover" type="text" @click="$router.replace({ path: '/' })">
-        <d2-icon name="home" style="font-size: 18px;"/>
+        <d2-icon name="home" style="font-size: 20px;"/>
       </el-button>
     </el-tooltip>
   </div>
