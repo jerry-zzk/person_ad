7819685e178c8fdb12e998911b2da53a79220a71
frank <18380462011@163.com>
2018-09-29 16:17:11 +0800

    客户黑名单

---
+++ src/pages/iblackList/page.vue
@@ -0,0 +1,35 @@
+<template>
+  <d2-container id="black_list" class="page">
+    123
+  </d2-container>
+</template>
+
+<script>
+export default {
+  name: 'black_list',
+  components: {
+     
+  },
+  data () {
+    return {
+      searchVal: '',
+      
+    }
+  },
+  mounted(){
+    
+  },
+  methods:{
+    
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~@/assets/style/public.scss';
+#black_list{
+  border-left: 1px solid #ccc;
+  background-color: #fff;
+  
+}
+</style>
